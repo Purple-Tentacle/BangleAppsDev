7ce01e9809484a1f75f2ebe9ccc2efddf5a277df
Purple-Tentacle <59914607+Purple-Tentacle@users.noreply.github.com>
2020-04-14 19:56:48 +0200

    first working widget

---
+++ apps/chronowid/app.js
@@ -0,0 +1,80 @@
+g.clear();
+Bangle.loadWidgets();
+Bangle.drawWidgets();
+
+const storage = require('Storage');
+const boolFormat = v => v ? "On" : "Off";
+let settings;
+
+function updateSettings() {
+  storage.write('chronowid.json', settings);
+}
+
+function resetSettings() {
+  settings = {
+    hours : 0,
+    minutes : 0,
+    seconds : 0,
+    started : false,
+  };
+  updateSettings();
+}
+
+settings = storage.readJSON('chronowid.json',1);
+if (!settings) resetSettings();
+
+function showMenu() {
+  const timerMenu = {
+    '': {
+      'title': 'Set timer',
+      'predraw': function() {
+        timerMenu.hours.value = settings.hours;
+        timerMenu.minutes.value = settings.minutes;
+        timerMenu.seconds.value = settings.seconds;
+        timerMenu.started.value = settings.started;
+      }
+    },
+    'Hours': {
+      value: settings.hours,
+      min: 0,
+      max: 24,
+      step: 1,
+      onchange: v => {
+        settings.hours = v;
+        updateSettings();
+      }
+    },
+    'Minutes': {
+      value: settings.minutes,
+      min: 0,
+      max: 59,
+      step: 1,
+      onchange: v => {
+        settings.minutes = v;
+        updateSettings();
+      }
+    },
+    'Seconds': {
+      value: settings.seconds,
+      min: 0,
+      max: 59,
+      step: 1,
+      onchange: v => {
+        settings.seconds = v;
+        updateSettings();
+      }
+    },
+    'Timer on': {
+      value: settings.started,
+      format: boolFormat,
+      onchange: v => {
+        settings.started = v;
+        updateSettings();
+      }
+      },
+    };
+  timerMenu['-Exit-'] =  ()=>{load();};
+  return E.showMenu(timerMenu);
+}
+
+showMenu();
---
+++ apps/chronowid/widget.js
@@ -0,0 +1,98 @@
+(() => {
+    var settings = {};
+    var height = 23;
+    var width = 40;
+    var counter = 0;
+    const storage = require('Storage');
+    var interval =  0;
+
+    var hours = 0,
+        minutes = 0,
+        seconds = 0,
+        ms = 0;
+
+    function printDebug() {
+        print ("Time: " + time);
+        print ("Counter: " + counter);
+        print ("Started: " + settings.started);
+        print ("----");
+    }
+
+    function countDown() {
+        printDebug();
+        if (counter > 0) {
+            if (settings.started) {
+                counter = counter - 1000;
+                time = msToTime(counter);
+                reload();
+            }
+        } else {
+            if (settings.started) {
+                Bangle.buzz(1500);
+                time = msToTime(counter);
+                clearInterval(interval);
+                interval = 0;
+
+                settings.started = false;
+                storage.write('chronowid.json', settings);
+                reload();
+            }
+        }
+    }
+
+    function msToTime(duration) {
+        var milliseconds = parseInt((duration % 1000) / 100),
+            seconds = Math.floor((duration / 1000) % 60),
+            minutes = Math.floor((duration / (1000 * 60)) % 60),
+            hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+
+        hours = (hours < 10) ? "0" + hours : hours;
+        minutes = (minutes < 10) ? "0" + minutes : minutes;
+        seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+        return hours + ":" + minutes + ":" + seconds;
+    }
+
+    function calcCounter() {
+        hours = settings.hours;
+        minutes =settings.minutes;
+        seconds = settings.seconds;
+        ms = (hours * 60 * 60 * 1000) + (minutes * 60 * 1000) + (seconds * 1000);
+        return ms;
+    }
+
+    // draw your widget
+    function draw() {
+        if (!settings.started) return;
+        g.reset();
+        g.clearRect(this.x,this.y,this.x+width,this.y+height);
+        g.drawString(time, this.x+1, this.y+(height/2));
+    }
+
+    function reload() {
+        settings = storage.readJSON("chronowid.json",1)||{};
+
+        if (settings.started) {
+            width = 40;
+        } 
+        else {
+            width = 0;
+        }
+        WIDGETS["chronowid"].draw();
+    }
+
+    settings = storage.readJSON("chronowid.json",1)||{};
+    counter = calcCounter();
+    time = msToTime(counter);
+
+    if (settings.started) interval = setInterval(countDown, 1000);
+
+    // add the widget
+    WIDGETS["chronowid"]={area:"tl",width:width,draw:draw,reload:function() {
+        reload();
+        Bangle.drawWidgets(); // relayout all widgets
+    }};
+
+    // load settings, set correct widget width
+    reload();
+})();
