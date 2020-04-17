22c41e2324e23fc779f07cfdcaf88392d4fc0555
Purple-Tentacle <59914607+Purple-Tentacle@users.noreply.github.com>
2020-04-17 22:12:39 +0200

    chnaged json array

--- apps/pedometer/app.js
+++ apps/pedometer/app.js
@@ -1,16 +1,32 @@
 const storage = require("Storage");
 const graph = require("graph");
-const dataFile = storage.open("activepedom.data.json", "r");
+var dataFile = storage.open("activepedom.data.json", "r");
 
 // initialize with default settings...
 var data = {
-    'time' : [],
-    'stepsCounted' : [],
-    'active' : [],
-    'stepsTooShort' : [],
-    'stepsTooLong' : [],
-    'stepsOutsideTime' : [],
+    'entries' : [{
+        'time' : 0,
+        'stepsCounted' : 0,
+        'active' : 0,
+        'stepsTooShort' : 0,
+        'stepsTooLong' : 0,
+        'stepsOutsideTime' : 0,
+    }]
 };
+
+//Convert ms to time
+function getTime(t)  {
+    var milliseconds = parseInt((t % 1000) / 100),
+    seconds = Math.floor((t / 1000) % 60),
+    minutes = Math.floor((t / (1000 * 60)) % 60),
+    hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+    
+    return hours + ":" + minutes + ":" + seconds;
+}
 
 function getData(file) {
     lines = [];
@@ -19,12 +35,15 @@ function getData(file) {
         if(nextLine) {
             lines.push(nextLine);
             dataSplitted = nextLine.split(',');
-            data.time[i] = dataSplitted[0];
-            data.stepsCounted[i] = dataSplitted[1];
-            data.active[i] = dataSplitted[2];
-            data.stepsTooShort[i] = dataSplitted[3];
-            data.stepsTooLong[i] = dataSplitted[4];
-            data.stepsOutsideTime[i] = dataSplitted[5];
+
+            data.entries.push({
+                "time" : dataSplitted[0], 
+                "stepsCounted" : dataSplitted[1],
+                "active" : dataSplitted[2],
+                "stepsTooShort" : dataSplitted[3],
+                "stepsTooLong" : dataSplitted[4],
+                "stepsOutsideTime" : dataSplitted[5]
+            });
         }
         i++;
     }
@@ -32,22 +51,32 @@ function getData(file) {
 }
 data = getData(dataFile);
 
-print(data.stepsTooLong);
-
-g.clear();
-g.setColor(0x07E0); //green
-graph.drawLine(g,data.stepsCounted, {
-    axes: true,
-    gridy : 1000,
-});
-
-g.setColor(0xFFFF); //white
-graph.drawLine(g,data.stepsOutsideTime, {
-    axes: true,
-    gridy : 1000,
-});
+print(data.entries[1]);
 
 data = undefined;
+dataFile = undefined;
+
+// now = new Date();
+// data.time.forEach(function(entry) {
+//     diff = now - entry;
+//     print (entry + " " + getTime(diff));
+//     print(getTime(entry));
+// });
+
+
+// g.clear();
+// g.setColor(0x07E0); //green
+// graph.drawLine(g,data.stepsCounted, {
+//     axes: true,
+//     gridy : 1000,
+// });
+
+// g.setColor(0xFFFF); //white
+// graph.drawLine(g,data.stepsOutsideTime, {
+//     axes: true,
+//     gridy : 1000,
+// });
+
 
   
   //Get all entries for a given date
