e2eb8e8bbdf0cbba39b551ae89488dfeaf34daa0
Purple-Tentacle <59914607+Purple-Tentacle@users.noreply.github.com>
2020-04-19 00:52:14 +0200

    second function for whole day graph

--- apps/pedometer/app.js
+++ apps/pedometer/app.js
@@ -1,15 +1,16 @@
 (() => {
 
 const storage = require("Storage");
-const history = 86400000; // 28800000=8h 43200000=12h //86400000=24h
+mode = "history";
+var history = 43200000; // 28800000=8h 43200000=12h //86400000=24h
 
 //Convert ms to time
 function getTime(t)  {
     date = new Date(t);
     offset = date.getTimezoneOffset() / 60;
     //var milliseconds = parseInt((t % 1000) / 100),
-    seconds = Math.floor((t / 1000) % 60),
-    minutes = Math.floor((t / (1000 * 60)) % 60),
+    seconds = Math.floor((t / 1000) % 60);
+    minutes = Math.floor((t / (1000 * 60)) % 60);
     hours = Math.floor((t / (1000 * 60 * 60)) % 24);
     hours = hours - offset;
     hours = (hours < 10) ? "0" + hours : hours;
@@ -37,27 +38,47 @@ function getArrayFromCSV(file, column) {
             dataSplitted = nextLine.split(','); //split line, 0= time, 1=stepsCounted, 2=active, 3=stepsTooShort, 4=stepsTooLong, 5=stepsOutsideTime
             diff = now - dataSplitted[0]; //calculate difference between now and stored time
             if (diff <= history) { //only entries from the last x ms
-                array.push(dataSplitted[column]);
+                    array.push(dataSplitted[column]);
             }
         }
         i++;
     }
     return array;
 }
+
+function getArrayFromCSV2(file, column) {
+    i = 0;
+    array = [];
+    now = new Date();
+    now.setDate(now.getDate() - 1);
+    while ((nextLine = file.readLine())) { //as long as there is a next line
+        if(nextLine) {
+            dataSplitted = nextLine.split(','); //split line, 0= time, 1=stepsCounted, 2=active, 3=stepsTooShort, 4=stepsTooLong, 5=stepsOutsideTime
+            date = new Date(dataSplitted[0]*1);
+            if (now.getDate() == date.getDate()) { //same day
+                    array.push(dataSplitted[column]);
+            }
+        }
+        i++;
+    }
+    return array;
+}
+
 var csvFile = storage.open("activepedom.data.json", "r");
-times = getArrayFromCSV(csvFile, 0);
+times = getArrayFromCSV2(csvFile, 0);
 start = getDate(times[0]) + " " + getTime(times[0]);
 stop =  getDate (times[times.length-1]) + " " + getTime(times[times.length-1]);
 
 var csvFile = storage.open("activepedom.data.json", "r");
-steps = getArrayFromCSV(csvFile, 1);
+steps = getArrayFromCSV2(csvFile, 1);
+print(steps);
 // actives = getArrayFromCSV(csvFile, 2);
 // shorts = getArrayFromCSV(csvFile, 3);
 // longs = getArrayFromCSV(csvFile, 4);
 // outsides = getArrayFromCSV(csvFile, 5); //array.push(dataSplitted[5].slice(0,-1));
 
 g.clear();
-require("graph").drawBar(g, steps, {
+require("graph").drawLine(g, steps, {
     title: "Steps Counted",
     axes : true,
     gridy : 1000,
