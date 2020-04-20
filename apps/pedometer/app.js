41f86e2a1265bb7ca5b4f7ec7c2e8f27093f7f09
Purple-Tentacle <59914607+Purple-Tentacle@users.noreply.github.com>
2020-04-20 17:27:53 +0200

    remmove mode, add timespan

--- apps/pedometer/app.js
+++ apps/pedometer/app.js
@@ -1,8 +1,7 @@
 (() => {
 
 const storage = require("Storage");
-var history = 43200000; // 28800000=8h 43200000=12h //86400000=24h
-var mode = "day";
+var history = 86400000; // 28800000=8h 43200000=12h //86400000=24h
 
 //Convert ms to time
 function getTime(t)  {
@@ -29,27 +28,17 @@ function getDate(t) {
     return year + "-" + month + "-" + day;
 }
 
-//modes: history=show entries from last ms defined in history, day=show entries from current day
 //columns: 0=time, 1=stepsCounted, 2=active, 3=stepsTooShort, 4=stepsTooLong, 5=stepsOutsideTime
-function getArrayFromCSV(file, column, mode) { 
+function getArrayFromCSV(file, column) { 
     i = 0;
     array = [];
     now = new Date();
     while ((nextLine = file.readLine())) { //as long as there is a next line
         if(nextLine) {
             dataSplitted = nextLine.split(','); //split line, 
-
-            if (mode == "history") {
-                diff = now - dataSplitted[0]; //calculate difference between now and stored time
-                if (diff <= history) { //only entries from the last x ms
-                        array.push(dataSplitted[column]);
-                }
-            }
-            if (mode == "day") {
-                date = new Date(dataSplitted[0]*1);
-                if (now.getDate() == date.getDate()) { //same day
-                        array.push(dataSplitted[column]);
-                }
+            diff = now - dataSplitted[0]; //calculate difference between now and stored time
+            if (diff <= history) { //only entries from the last x ms
+                    array.push(dataSplitted[column]);
             }
         }
         i++;
@@ -57,14 +46,18 @@ function getArrayFromCSV(file, column, mode) {
     return array;
 }
 
-function drawGraph(mode) {
+function drawGraph() {
     //times
     // actives = getArrayFromCSV(csvFile, 2);
     // shorts = getArrayFromCSV(csvFile, 3);
     // longs = getArrayFromCSV(csvFile, 4);
     // outsides = getArrayFromCSV(csvFile, 5); //array.push(dataSplitted[5].slice(0,-1));
-    var csvFile = storage.open("activepedom.data.json", "r");
-    times = getArrayFromCSV(csvFile, 0, mode);
+    now = new Date();
+    month = now.getMonth() + 1;
+    if (month < 10) month = "0" + month;
+    filename = filename = "activepedom-" + now.getFullYear() + month + now.getDate() + ".data";
+    var csvFile = storage.open(filename, "r");
+    times = getArrayFromCSV(csvFile, 0);
     first = getDate(times[0]) + " " + getTime(times[0]);
     last =  getDate (times[times.length-1]) + " " + getTime(times[times.length-1]);
     //free memory
@@ -72,8 +65,8 @@ function drawGraph(mode) {
     times = undefined;
 
     //steps
-    var csvFile = storage.open("activepedom.data.json", "r");
-    steps = getArrayFromCSV(csvFile, 1, mode);
+    var csvFile = storage.open(filename, "r");
+    steps = getArrayFromCSV(csvFile, 1);
     //define y-axis grid labels
     stepsLastEntry = steps[steps.length-1];
     if (stepsLastEntry < 1000) gridyValue = 100;
@@ -101,22 +94,29 @@ function drawGraph(mode) {
 function drawMenu () {
     g.clear();
     g.setFont("6x8", 1);
-    g.drawString("BTN1:Mode | BTN2:Draw | BTN3:Span ", 20, 10);
-    if (mode == "day") g.drawString("Mode: " + mode, 20, 20);
-    if (mode == "history") g.drawString("Mode: " + mode + " | Timespan: " + history/1000/60/60 + " hours", 20, 20);
+    g.drawString("BTN1:Timespan | BTN2:Draw", 20, 10);
+    g.drawString("Timespan: " + history/1000/60/60 + " hours", 20, 20);
 }
 
 setWatch(function() { //BTN1
-    if (mode == "day") mode = "history";
-        else mode = "day";
+    switch(history) {
+        case 3600000 : //1h
+            history = 14400000; //4h
+            break;
+        case 86400000 : //24
+            history = 3600000; //1h
+            break;
+        default : 
+            history = history + 14400000; //4h
+            break;
+    }
     drawMenu();
 }, BTN1, {edge:"rising", debounce:50, repeat:true});
 
 setWatch(function() { //BTN2
     g.setFont("6x8", 2);
     g.drawString ("Drawing...",30,60);
-    //modes: history=show entries from last ms defined in history, day=show entries from current day
-    drawGraph(mode);
+    drawGraph();
 }, BTN2, {edge:"rising", debounce:50, repeat:true});
 
 setWatch(function() { //BTN3
